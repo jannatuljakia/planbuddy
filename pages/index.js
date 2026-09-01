3f9651fce00e7a4cf0705c860d4772657a3d8ea0
jannatuljakia <jannatuljakia1234@gmail.com>
2026-09-01 19:49:46 +0600

    Update index.js
    Updated the title in the Head component and added a favicon.

--- pages/index.js
+++ pages/index.js
@@ -1086,10 +1086,11 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>
-          💜 PlanBuddy
-        </title>
-      </Head>
+  <title>
+  PlanBuddy
+  </title>
+  <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>%F0%9F%92%9C</text></svg>" />
+</Head>
 
       <div
         className="app"
