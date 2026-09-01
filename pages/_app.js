6f854e6492f93e8ae858d18bce581657b09877d3
jannatuljakia <jannatuljakia1234@gmail.com>
2026-09-02 00:14:06 +0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import '../styles/globals.css';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/sw.js')
+        .then((reg) => console.log('SW registered:', reg.scope))
+        .catch((err) => console.error('SW failed:', err));
+    }
+  }, []);
+
   return <Component {...pageProps} />;
 }
